48e6d51ef4f6233661efee8d7b9f8cc3e11bc15f
Matt Jones <git@mattjones.technology>
2021-02-28 12:35:14 -0600

    Add the java debugger to VS Code.

--- Java-Programming-MB/setupCourse.sh
+++ Java-Programming-MB/setupCourse.sh
@@ -1,2 +1,8 @@
 mkdir /code
 cd /code
+
+#intsall the java extension.
+apt install bsdtar -y
+curl -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/vscjava/vsextensions/vscode-java-debug/0.31.0/vspackage | bsdtar -xvf - extension
+mv extension /opt/.katacodacode/extensions/msjavadebugger
+
